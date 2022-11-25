80ba50d9910923b59e426d95c051aa9d8a97e76a
EddyShimwa <shedpray16@gmail.com>
2022-11-25 19:28:57 +0200

    Added desktop version

--- README.md
+++ README.md
@@ -1,42 +1,160 @@
-![](https://img.shields.io/badge/Microverse-blueviolet)
+<a name="readme-top"></a>
 
-#page
+<div align="center">
+
+  <img src="./Images/murple_logo.png" alt="logo" width="140"  height="auto" />
+  <br/>
+
+</div>
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Tech Stack](#tech-stack)
+    - [Key Features](#key-features)
+  - [🚀 Live Demo](#live-demo)
+- [💻 Getting Started](#getting-started)
+  - [Setup](#setup)
+  - [Prerequisites](#prerequisites)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Run tests](#run-tests)
+  - [Deployment](#triangular_flag_on_post-deployment)
+- [👥 Authors](#authors)
+- [🔭 Future Features](#future-features)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [❓ FAQ](#faq)
+- [📝 License](#license)
+
+# 📖 [Creative Commons Global Summit] <a name="about-project"></a>
+
+> 
+
+**[Creative Commons Global Summit]** Is a first capstone project in microverse, It includes all the lessons we learned in 4 weeks including HTML, javaScript, CSS, bootstrap 
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+<details>
+  <summary>Client</summary>
+  <ul>
+    <li><a href="">HTML</a></li>
+    <li><a href="">CSS</a></li>
+    <li><a href="">javaScript</a></li>
+  </ul>
+</details>
+
+### Key Features <a name="key-features"></a>
+
+- **[Completed Mobile version]**
+- **[Completed Desktop version]**
+- **[Added functionalities on menu buttons for mobile version]**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+- [Live Demo Link](https://eddyshimwa.github.io/CapStone-Project/)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+### Prerequisites
+
+### Setup
+
+Clone this repository to your desired folder:
 
 
-## Built With
+### Install
 
-- HTML
-- CSS
-- JavaScript
-## Link
-https://eddyshimwa.github.io/Portforio/
+### Usage
 
-## Link
-https://eddyshimwa.github.io/Portforio/
 
-👤 **Author**
+
+### Run tests
+
+
+
+### Deployment
+
+You can deploy this project using:
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 👥 Authors 
+
+👤 **Author1**
 
 - GitHub: [@EddyShimwa](https://github.com/EddyShimwa)
 - Twitter: [@eddy56388406](https://twitter.com/eddy56388406)
-- LinkedIn: [Pray Eddy SHIMWA]([https://www.linkedin.com/in/pray-eddy-shimwa-18a6751b6/]
+- LinkedIn: [Pray Eddy SHIMWA]([https://www.linkedin.com/in/pray-eddy-shimwa-18a6751b6/])
 
 
-## 🤝 Contributing
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+## 🔭 Future Features <a name="future-features"></a>
+
+> Describe 1 - 3 features you will add to the project.
+
+- [ ] **[new_feature_1]**
+- [ ] **[new_feature_2]**
+- [ ] **[new_feature_3]**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+## 🤝 Contributing <a name="contributing"></a>
 
 Contributions, issues, and feature requests are welcome!
 
 Feel free to check the [issues page](../../issues/).
 
-## Show your support
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
 
-Give a ⭐️ if you like this project!
+## ⭐️ Show your support <a name="support"></a>
 
-## Acknowledgments
+> Write a message to encourage readers to support your project
 
-- Hat tip to anyone whose code was used
-- Inspiration
-- etc
+If you like this project...
 
-## 📝 License
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+> Give credit to everyone who inspired your codebase.
+
+I would like to thank...
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+## ❓ FAQ <a name="faq"></a>
+
+> Add at least 2 questions new developers would ask when they decide to use your project.
+
+- **[Question_1]**
+
+  - [Answer_1]
+
+- **[Question_2]**
+
+  - [Answer_2]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 📝 License <a name="license"></a>
 
 This project is [MIT](./LICENSE) licensed.
+
+_NOTE: we recommend using the [MIT license](https://choosealicense.com/licenses/mit/) - you can set it up quickly by [using templates available on GitHub](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/adding-a-license-to-a-repository). You can also use [any other license](https://choosealicense.com/licenses/) if you wish._
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- index.js
+++ index.js
@@ -1,81 +1,80 @@
-const hamburger = document.querySelector(".hamburger");
-const crossIcon = document.querySelector(".Cross-Icon");
-const menuItems = document.querySelector(".menu-items");
+const hamburger = document.querySelector('.hamburger');
+const crossIcon = document.querySelector('.Cross-Icon');
+const menuItems = document.querySelector('.menu-items');
 
 function navToggle() {
-  menuItems.classList.toggle("df");
+  menuItems.classList.toggle('df');
 }
 
-hamburger.addEventListener("click", navToggle);
-crossIcon.addEventListener("click", navToggle);
+hamburger.addEventListener('click', navToggle);
+crossIcon.addEventListener('click', navToggle);
 
-var speakers = [
+const speakers = [
   {
-    Image: "/Images/esem.jpg",
-    title: "well well",
-    name: "Esmeralda Montana",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sapiente modi voluptatibus!.",
+    Image: './Images/speaker-4.png',
+    title: 'General manager',
+    name: 'Esmeralda Montana',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
-    Image: "/Images/PASSS.jpg",
-    title: "tiltle",
-    name: "Name",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi fugit quia aliquid illo veniam",
+    Image: './Images/speaker-1.png',
+    title: 'Vice-Director ',
+    name: 'Becker Allison',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
-    Image: "/Images/shi.jpg",
-    title: "tiltle",
-    name: "Name",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi fugit quia aliquid illo veniam",
+    Image: './Images/speaker-2.png',
+    title: 'General Director',
+    name: 'Frank Ghosh',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
-    Image: "/Images/PASSS.jpg",
-    title: "tiltle",
-    name: "Name",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi fugit quia aliquid illo veniam",
+    Image: './Images/speaker-3.png',
+    title: 'Executive manager',
+    name: 'Claudine Besker',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
 ];
-const moreBtn = document.createElement("div");
-moreBtn.id = "more-btn";
-const spanBtn = document.createElement("span");
-moreBtn.innerText = "MORE";
+const moreBtn = document.createElement('div');
+moreBtn.id = 'more-btn';
+const spanBtn = document.createElement('span');
+moreBtn.innerText = 'MORE';
 moreBtn.appendChild(spanBtn);
 
-const downIcon = document.createElement("img");
-(downIcon.src = "/Images/down-button.png"), moreBtn.appendChild(downIcon);
-moreBtn.style.display = "none";
+const downIcon = document.createElement('img');
+downIcon.src = '/Images/down-button.png';
+moreBtn.appendChild(downIcon);
+moreBtn.style.display = 'none';
 
-const speakersContainer = document.querySelector("#speakers");
+const speakersContainer = document.querySelector('#speakers');
 
 for (let i = 0; i < speakers.length; i += 1) {
-  console.log(speakers[i].title);
+  const speaker = document.createElement('div');
+  speaker.classList = 'speaker';
+  const imageDiv = document.createElement('div');
+  imageDiv.classList = 'image';
 
-  const speaker = document.createElement("div");
-  speaker.classList = "speaker";
-  const imageDiv = document.createElement("div");
-  imageDiv.classList = "image";
-
-  const image = document.createElement("img");
+  const image = document.createElement('img');
   image.src = speakers[i].Image;
   imageDiv.appendChild(image);
   speaker.appendChild(imageDiv);
 
-  const details = document.createElement("div");
-  details.classList = "Details";
+  const details = document.createElement('div');
+  details.classList = 'Details';
 
-  const speakerName = document.createElement("h3");
+  const speakerName = document.createElement('h3');
   details.appendChild(speakerName);
 
   speakerName.innerText = speakers[i].name;
 
-  const italics = document.createElement("i");
+  const italics = document.createElement('i');
   italics.innerText = speakers[i].title;
   details.appendChild(italics);
 
-  const horizontalLine = document.createElement("hr");
+  const horizontalLine = document.createElement('hr');
   details.appendChild(horizontalLine);
 
-  const description = document.createElement("p");
+  const description = document.createElement('p');
   details.appendChild(description);
 
   description.innerText = speakers[i].description;
@@ -83,37 +82,34 @@ for (let i = 0; i < speakers.length; i += 1) {
   speaker.appendChild(details);
 
   speakersContainer.appendChild(speaker);
-  speakers[i];
 }
 
-var allSpeakers = document.getElementsByClassName("speaker");
+const allSpeakers = document.getElementsByClassName('speaker');
 
-console.log(allSpeakers);
-
-var speakersToShow = 2;
-var numberOfSpeakers = speakers.length;
-for (let i = 0; i < allSpeakers.length; i++) {
-  allSpeakers[i].style.display = "none";
+const speakersToShow = 4;
+const numberOfSpeakers = speakers.length;
+for (let i = 0; i < allSpeakers.length; i += 1) {
+  allSpeakers[i].style.display = 'none';
   if (i < speakersToShow) {
-    allSpeakers[i].style.display = "flex";
+    allSpeakers[i].style.display = 'flex';
   }
 }
 
 if (numberOfSpeakers > speakersToShow) {
-  moreBtn.style.display = "block";
+  moreBtn.style.display = 'block';
 }
 
-moreBtn.addEventListener('click', function () {
-  var showing = Array.from(allSpeakers).filter((s,i) => allSpeakers[i].style.display === "flex").length;
-//   for(let i)
-  for (var i = showing - 1; i < showing + speakersToShow; i++) {
-    allSpeakers[i].style.display = "flex";
+moreBtn.addEventListener('click', () => {
+  const showing = Array.from(allSpeakers).filter((s, i) => allSpeakers[i].style.display === 'flex').length;
+  //   for(let i)
+  for (let i = showing - 1; i < showing + speakersToShow; i += 1) {
+    allSpeakers[i].style.display = 'flex';
   }
-  var nowShowing = Array.from(allSpeakers).filter(
-    (s,i) => allSpeakers[i].style.display === "flex"
+  const nowShowing = Array.from(allSpeakers).filter(
+    (s, i) => allSpeakers[i].style.display === 'flex',
   ).length;
   if (nowShowing >= numberOfSpeakers) {
-    moreBtn.style.display = "none";
+    moreBtn.style.display = 'none';
   }
 });
 speakersContainer.appendChild(moreBtn);
